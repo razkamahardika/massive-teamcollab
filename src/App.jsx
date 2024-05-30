7f2e59d0c457804295aded587a38b9d0e00d493e
fauziah <fauziahzahra2002@gmail.com>
2024-05-30 22:26:44 +0700

    fauziah

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import ProductPage from './pages/ProductPage'
-import Chart from './pages/Chart'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
+import Chart from "./pages/Chart";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Wishlist from "./pages/Wishlist";
+import "./App.css";
 
 function App() {
   return (
@@ -12,10 +15,13 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/productpage" element={<ProductPage />} />
           <Route path="/chart" element={<Chart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
