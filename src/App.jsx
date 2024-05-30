b10826b0cdcd118ea4a0f74641f0b08668e54ddd
razkamahardika <razka.mahardika@mhs.itenas.ac.id>
2024-05-30 23:22:09 +0700

    undo-merge-fau

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import ProductPage from "./pages/ProductPage";
-import Chart from "./pages/Chart";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Wishlist from "./pages/Wishlist";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ProductPage from './pages/ProductPage'
+import Chart from './pages/Chart'
+import './App.css'
 
 function App() {
   return (
@@ -15,13 +12,10 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/productpage" element={<ProductPage />} />
           <Route path="/chart" element={<Chart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
