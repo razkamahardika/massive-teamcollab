378601b5779e3ea8d0d7c2440aae28d1ca2ea184
maulanario <maulanario790@gmail.com>
2024-06-03 21:33:42 +0700

    rioslebew

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage'
 import Chart from './pages/Chart'
+import DetailProduk from './pages/DetailProduk'
+import Payment from './pages/Payment'
 import './App.css'
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/productpage" element={<ProductPage />} />
           <Route path="/chart" element={<Chart />} />
+          <Route path="/detailproduk" element={<DetailProduk/>} />
+          <Route path="/payment" element={<Payment/>} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import Header from '../components/Header'
+import '../assets/pages/Chart.css'
 
 const Chart = () => {
   return (
-    <div>
-      <h1>Razka Slebew</h1>
+    <>
+    <Header />
+    
+    <div className="app">
+      <div className="title-container">
+        <h1 className="title">Your Chart</h1>
+      </div>
+      <div className="content">
+        <div className="table-container">
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Details</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Example Product</td>
+                <td>Example Details</td>
+                <td>1</td>
+                <td>$100</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="summary">
+            <h2>Summary</h2>
+            <hr />
+            <div className="summary-item">
+              <span>Sub-total</span>
+              <span>Rp.3.000.000</span>
+            </div>
+            <div className="summary-item">
+              <span>Tax</span>
+              <span>Rp.300.000</span>
+            </div>
+            <div className="summary-item">
+              <span>Total</span>
+              <span>Rp.3.300.000</span>
+            </div>
+            <hr />
+            <center>
+              <button className="button">Checkout</button>
+            </center>
+          </div>
+        </div>
+      </div>
+      <div className="whatsapp-icon">
+        <a href="https://wa.me/your-number" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-whatsapp"></i>
+        </a>
+      </div>
     </div>
-  )
-}
+
+    </>
+
+  );
+};
+
+
 
 export default Chart
